d7d7ce46d6a28419ff3c7c9414864943dfddd1a2
AndyS1mpson <whale942999@gmail.com>
2022-03-09 20:28:18 +0300

    add viola-jonse

--- Task1/gui/detecotrs.py
+++ Task1/gui/detecotrs.py
@@ -1,6 +1,4 @@
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 def template_matching(method) -> None:
@@ -23,4 +21,17 @@ def template_matching(method) -> None:
       bottom_right = (top_left[0] + w, top_left[1] + h)
       cv2.rectangle(img, top_left, bottom_right, 255, 2)
       cv2.imwrite("images/result.jpg", img)
-      #plt.subplot(122),plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+
+def viola_jones() -> None:
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    image = cv2.imread("images/img.jpg")
+    img = image.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    cv2.imwrite("images/result.jpg", img)
--- Task1/gui/main.py
+++ Task1/gui/main.py
@@ -1,15 +1,14 @@
 from tkinter import Button, Label, Radiobutton, StringVar, Tk, ttk, filedialog
-from turtle import width
 from PIL import Image, ImageTk
 
-from detecotrs import template_matching
+from detecotrs import template_matching, viola_jones
 
 
 def save_image():
     global image
     im = Image.open(filedialog.askopenfilename(title='open'))
     im.save("images/img.jpg")
-    im = im.resize((400, 300))
+    im = im.resize((500, 400))
     image = ImageTk.PhotoImage(im)
     image_label = Label(templ_match_tab, image=image)
     image_label.grid(row=7, column=0, padx=15, pady=10)
@@ -18,7 +17,7 @@ def save_template():
     global templ
     im = Image.open(filedialog.askopenfilename(title='open'))
     im.save("images/template.jpg")
-    im = im.resize((200, 200))
+    im = im.resize((300, 200))
     templ = ImageTk.PhotoImage(im)
     template_label = Label(templ_match_tab, image=templ)
     template_label.grid(row=7, column=1, padx=15, pady=10)
@@ -27,11 +26,29 @@ def tm_result():
     global result_image
     template_matching(method.get())
     result = Image.open("images/result.jpg")
-    result.save("images/result.jpg")
-    result = result.resize((400, 300))
+    #result.save("images/result.jpg")
+    result = result.resize((500, 400))
     result_image = ImageTk.PhotoImage(result)
     result_label = Label(templ_match_tab, image=result_image)
     result_label.grid(row=7, column=2, padx=15, pady=10)   
+
+def vj_save_image():
+    global v_image
+    im = Image.open(filedialog.askopenfilename(title='open'))
+    im.save("images/img.jpg")
+    im = im.resize((500, 500))
+    v_image = ImageTk.PhotoImage(im)
+    image_label = Label(viola_jones_tab, image=v_image)
+    image_label.grid(row=1, column=0, padx=15, pady=10)
+
+def vj_result():
+    global v_result_image
+    viola_jones()
+    result = Image.open("images/result.jpg")
+    result = result.resize((500, 500))
+    v_result_image = ImageTk.PhotoImage(result)
+    v_result_label = Label(viola_jones_tab, image=v_result_image)
+    v_result_label.grid(row=1, column=1, padx=15, pady=10) 
 
 
 window = Tk()
@@ -54,20 +71,28 @@ tm_sqdiff = Radiobutton(templ_match_tab, text="TM_SQDIFF", variable=method, valu
 tm_ccorr = Radiobutton(templ_match_tab, text="TM_CCORR",  variable=method, value='cv2.TM_CCORR')
 tm_ccoeff = Radiobutton(templ_match_tab, text="TM_CCOEFF",  variable=method, value='cv2.TM_CCOEFF')
 
-# Add buttons
+# Add buttons for TM
 img_but = Button(templ_match_tab, text="Загрузить изображение", command = lambda : save_image())
 template_but = Button(templ_match_tab, text="Загрузить шаблон", command = lambda : save_template())
 res_but = Button(templ_match_tab, text="Получить результат", command = lambda : tm_result())
+
+# Add buttons for Viola Jones
+v_img_button = Button(viola_jones_tab, text="Загрузить фотографию", command = lambda : vj_save_image())
+v_res_but = Button(viola_jones_tab, text="Получить результат", command = lambda : vj_result())
 
 # Add radio buttons to window
 tm_sqdiff.grid(row=0, column=0, padx=15, pady=7)
 tm_ccorr.grid(row=1, column=0, padx=15, pady=7)
 tm_ccoeff.grid(row=2, column=0, padx=15, pady=7)
 
-# Add buttons to window
+# Add buttons to TM window
 img_but.grid(row=4, column=0, padx=15, pady=10)
 template_but.grid(row=4, column=1, padx=15, pady=10)
 res_but.grid(row=4, column=2, padx=15, pady=10)
+
+# ADD buttons to VJ window
+v_img_button.grid(row=0, column=0, padx=15, pady=7)
+v_res_but.grid(row=0, column=1, padx=15, pady=7)
 
 # Add tabs to window
 tab_parent.add(templ_match_tab, text="Template Matching")
